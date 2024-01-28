9c3fe1de3cec531f318fe8f1b2ca9bff7fae365f
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-01-28 23:37:54 +0530

    DSA with java Patter

--- com/dailycode/dsa_05_loops.java
+++ com/dailycode/dsa_05_loops.java
@@ -12,11 +12,18 @@ public class dsa_05_loops {
 //
 //        }
 //        System.out.println(sum);
-        for(int i = 1;i<=5; i++){
-            for(int j = 1; j<=i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
+//        for(int i = 1;i<=5; i++){
+//            for(int j = 1; j<=i; j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int n  = sc.nextInt();
+        int sum =0;
+        for(int i =1; i<=10; i++){
+            System.out.println(n+" X "+ i+" = "+ n*i);
         }
 
     }
