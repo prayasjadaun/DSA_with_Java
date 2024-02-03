e7755a7b3886344ff4c6ed6faa748bdab5e3a2a8
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-03 22:11:31 +0530

    Array in java practice question

--- com/dailycode/dsa_10_array.java
+++ com/dailycode/dsa_10_array.java
@@ -1,21 +1,46 @@
 package com.dailycode;
+
 import java.util.*;
 
 public class dsa_10_array {
-    public static void main(String[]args){
+    public static void main(String[] args) {
+//  Question 1--Take an array of names as input from the user and print them on the screen.
+//        Scanner sc = new Scanner(System.in);
+//        int size = 5;
+//
+//        String names[] = new String[size];
+//        //Input
+//        for(int i = 0; i<size; i++){
+//            System.out.println("Enter ypur names: ");
+//            names[i] = sc.next();
+//        }
+//        //Output
+//        for(int i = 0; i< names.length; i++){
+//            System.out.println("Name "+ (i+1)+ " is: "+ names[i]);
+//        }
+
+//Question 2-- Find the maximum & minimum number in an array of integers.
         Scanner sc = new Scanner(System.in);
         int size = 5;
-
-        String names[] = new String[size];
+        int[] numbers = new int[size];
         //Input
-        for(int i = 0; i<size; i++){
-            System.out.println("Enter ypur names: ");
-            names[i] = sc.next();
+        for (int i = 0; i < size; i++) {
+            numbers[i] = sc.nextInt();
         }
-        //Output
-        for(int i = 0; i< names.length; i++){
-            System.out.println("Name "+ (i+1)+ " is: "+ names[i]);
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
         }
+        System.out.println("Largest number is: " + max);
+        System.out.println("Smallest number is: " + min);
 
     }
 }
