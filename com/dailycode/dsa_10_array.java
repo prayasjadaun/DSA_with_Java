95dd703093177e9fd2c867aee3b401bc1ba6068c
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-03 22:29:32 +0530

    Array in java practice question

--- com/dailycode/dsa_10_array.java
+++ com/dailycode/dsa_10_array.java
@@ -20,27 +20,50 @@ public class dsa_10_array {
 //        }
 
 //Question 2-- Find the maximum & minimum number in an array of integers.
+//        Scanner sc = new Scanner(System.in);
+//        int size = 5;
+//        int[] numbers = new int[size];
+//        //Input
+//        for (int i = 0; i < size; i++) {
+//            numbers[i] = sc.nextInt();
+//        }
+//
+//        int min = Integer.MAX_VALUE;
+//        int max = Integer.MIN_VALUE;
+//
+//        for (int i = 0; i < numbers.length; i++) {
+//            if (numbers[i] < min) {
+//                min = numbers[i];
+//            }
+//            if (numbers[i] > max) {
+//                max = numbers[i];
+//            }
+//        }
+//        System.out.println("Largest number is: " + max);
+//        System.out.println("Smallest number is: " + min);
+
+// Question 3-- Take an array of numbers as input and check if it is an array sorted in ascending order.
+//              Eg : { 1, 2, 4, 7 } is sorted in ascending order.
+//              {3, 4, 6, 2} is not sorted in ascending order.
         Scanner sc = new Scanner(System.in);
         int size = 5;
-        int[] numbers = new int[size];
-        //Input
-        for (int i = 0; i < size; i++) {
-            numbers[i] = sc.nextInt();
-        }
+        int[] number = new int[size];
+         number = new int[]{2,3,4,5,6};
+//        Input
+//        for (int i = 0; i < size; i++) {
+//            number[i] = sc.nextInt();
+//        }
 
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
-
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] < min) {
-                min = numbers[i];
-            }
-            if (numbers[i] > max) {
-                max = numbers[i];
+        boolean isAscending = true;
+        for (int i = 0; i < number.length - 1; i++) {
+            if (number[i] > number[i + 1]) {
+                isAscending = false;
             }
         }
-        System.out.println("Largest number is: " + max);
-        System.out.println("Smallest number is: " + min);
-
+        if (isAscending) {
+            System.out.println("The array is sorted in ascending order");
+        } else {
+            System.out.println("The array is not sorted in ascending order");
+        }
     }
 }
