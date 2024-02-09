165379e2f4f03a3e31602d9fb0a36658c7fa841c
Thakur Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-09 22:50:36 +0530

    String Builder in java

---
+++ com/dailycode/dsa_13_StringBuilder.java
@@ -0,0 +1,28 @@
+package com.dailycode;
+import java.util.*;
+
+import com.sun.security.jgss.GSSUtil;
+
+public class dsa_13_StringBuilder {
+    public static void main(String[]args){
+        StringBuilder sb = new StringBuilder("Prayas");
+//        System.out.println(sb);
+//        // char at index 0
+//        System.out.println(sb.charAt(0));
+//
+//      //  set char at index 0
+//        sb.setCharAt(0,'R');
+//        System.out.println(sb);
+//        // insert the index
+//        sb.insert(0, 'S');
+//        System.out.println(sb);
+////        delete the index
+//        sb.delete(2, 4);
+//        System.out.println(sb);
+
+
+
+
+
+    }
+}
