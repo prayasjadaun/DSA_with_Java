12491223b03914e92335a34fab22e0711f54a65d
Prayas Jadaun <prayashthakur6@gmail.com>
2024-02-18 00:01:39 +0530

    permutation String in java

--- com/dailycode/dsa_16_PermutationString.java
+++ com/dailycode/dsa_16_PermutationString.java
@@ -1,4 +1,5 @@
 package com.dailycode;
+/*
 
 public class dsa_16_PermutationString {
     public static void printPerm(String str, String permutation){
@@ -16,5 +17,44 @@ public class dsa_16_PermutationString {
     public static void main(String[]args){
         String str = "abc";
         printPerm(str, "");
+    }
+}
+ */
+
+public class dsa_16_PermutationString {
+   /*
+    public static int countPaths(int i , int j , int n, int m){
+        if(i==n || j==m){
+            return 0;
+        }
+        if(i==n-1 && j ==m-1){
+            return 1;
+        }
+        //Move Downwards
+        int downPaths = countPaths(i+1,j,n,m);
+        //Move Right
+        int rightPaths = countPaths(i, j+1, n, m);
+        return downPaths+ rightPaths;
+    }
+    public static void main(String[]args){
+        int n =3 , m = 3;
+        int totalPaths = countPaths(0,0,n,m);
+        System.out.println(totalPaths);
+
+    }
+    */
+
+    public static int callGuests(int n){
+        if(n<=1){
+            return 1;
+        }
+        int way1 = callGuests(n-1);
+
+        int way2 = (n-1) * callGuests(n-2);
+        return way1 + way2;
+    }
+    public static void main(String[]args){
+        int n = 4;
+        System.out.println(callGuests(n));
     }
 }
